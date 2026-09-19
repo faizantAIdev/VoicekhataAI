cc5f27338a3ca54fb1077e4098dc1fd9a166bf9c
FaizanCoder122 <145524634+FaizanCoder122@users.noreply.github.com>
2026-09-19 14:35:32 +0530

    time chacker

--- src/app.js
+++ src/app.js
@@ -9,6 +9,19 @@ const authRoutes = require('./routes/authRoutes');
 const voiceRoutes = require('./routes/voiceRoutes');
 const app = express();
 
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const time = Date.now() - start;
+
+    console.log(
+      `⏱️ ${req.method} ${req.originalUrl} → ${res.statusCode} → ${time}ms`
+    );
+  });
+
+  next();
+});
 // ==========================================
 // MIDDLEWARE
 // ==========================================
